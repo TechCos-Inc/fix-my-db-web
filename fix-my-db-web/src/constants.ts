d7d6a94bae65b7d93ae0f51ff65a3e8e7362dc76
HarishK <harish@letztrip.com>
2025-06-22 22:37:31 +0530

    fix : "Support Mail"

--- fix-my-db-web/src/constants.ts
+++ fix-my-db-web/src/constants.ts
@@ -2,5 +2,5 @@
 
 export const APP_NAME = 'FixMyDB';
 export const TAGLINE = 'Reliable. Optimized. Always Ready.';
-export const SUPPORT_EMAIL = 'support@fixmydb.com';
+export const SUPPORT_EMAIL = 'technical-team@fixmydb.com';
 export const COPYRIGHT_YEAR = new Date().getFullYear();
